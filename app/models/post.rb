e9fbbba1e67d65ca367d10275dcb0306abceabb2
Saloum Jobe <jobajubauk@gmail.com>
2017-04-13 22:44:59 +0000

    Zoom distortment sorted for home page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: 67000} #I am just a test. Delete me later. Test capped at 800 characters.
+  validates :content, presence: true
   
   default_scope -> { order(created_at: :desc) } #newest posts show first on timeline
 end
